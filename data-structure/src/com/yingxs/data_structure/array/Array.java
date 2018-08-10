ab7831775a788c612c4afe5e9458977eac4c52c6
王昊 <yingxs888@126.com>
2018-08-10 22:23:13 +0800

    fix (Array):防止addLast和removeLast复杂度震荡
    当size==capacity/4时，才将capacity减半

--- data-structure/src/com/yingxs/data_structure/array/Array.java
+++ data-structure/src/com/yingxs/data_structure/array/Array.java
@@ -155,7 +155,7 @@ public class Array<E> {
 		size--;
 		data[size] = null;
 		
-		if(size == data.length / 2)
+		if(size == data.length / 4 && data.length / 2 != 0 )
 			resize(data.length / 2);
 		
 		
