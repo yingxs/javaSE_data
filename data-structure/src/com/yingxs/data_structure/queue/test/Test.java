cb94c1bd02f50ec0a577486df1e2a33e5dd4d57e
王昊 <yingxs888@126.com>
2018-08-13 23:10:43 +0800

    feat (LoopQueue):数组(非动态数组)实现循环队列，出队时间复杂度为O(1)

--- data-structure/src/com/yingxs/data_structure/queue/test/Test.java
+++ data-structure/src/com/yingxs/data_structure/queue/test/Test.java
@@ -1,10 +1,28 @@
 package com.yingxs.data_structure.queue.test;
 
 import com.yingxs.data_structure.queue.ArrayQueue;
+import com.yingxs.data_structure.queue.LoopQueue;
 
 public class Test {
 	public static void main(String[] args) {
+//		test1();
+		test2();
+	}
+	
+	public static void test1(){
 		ArrayQueue<Integer> queue = new ArrayQueue<Integer>();
+		for(int i = 0 ;i < 10 ;i++){
+			queue.enqueue(i);
+			System.out.println(queue);
+			
+			if( i % 3 == 2){
+				queue.dequeue();
+				System.out.println(queue);
+			}
+		}
+	}
+	public static void test2(){
+		LoopQueue<Integer> queue = new LoopQueue<Integer>();
 		for(int i = 0 ;i < 10 ;i++){
 			queue.enqueue(i);
 			System.out.println(queue);
