6b17d8408991bd02c91f445a3ead6a48140232c5
王昊 <yingxs888@126.com>
2018-08-16 19:38:27 +0800

    feat (LinkedListStack):使用链表实现栈结构O(n)
    使用链表和数组分别实现栈结构，当数据量不同时，两者性能会有差异，但差异不大；两种实现方式都是O(1)的时间复杂度，有较小差异属于正常现象，LinkedListStack会有较多的new操作可能导致数据量大时，性能差于数组实现

--- data-structure/src/com/yingxs/data_structure/stack/test/Test.java
+++ data-structure/src/com/yingxs/data_structure/stack/test/Test.java
@@ -1,11 +1,30 @@
 package com.yingxs.data_structure.stack.test;
 
+import java.util.Random;
+
 import com.yingxs.data_structure.stack.ArrayStack;
+import com.yingxs.data_structure.stack.LinkedListStack;
+import com.yingxs.data_structure.stack.Stack;
 
 public class Test {
 	public static void main(String[] args) {
 		
+		test3();
+	}
+	
+	
+	public static void test1(){
 		ArrayStack<Integer> stack = new ArrayStack<Integer>();
+		for(int i = 0 ;i < 5;i++){
+			stack.push(i);
+			System.out.println(stack);
+		}
+		stack.pop();
+		System.out.println(stack);
+	}
+	
+	public static void test2(){
+		LinkedListStack<Integer> stack = new LinkedListStack<Integer>();
 		for(int i = 0 ;i < 5;i++){
 			stack.push(i);
 			System.out.println(stack);
@@ -14,4 +33,38 @@ public class Test {
 		System.out.println(stack);
 		
 	}
+	
+	
+	public static double testStack(Stack<Integer> stack, int opCount){
+		
+		long startTime = System.nanoTime();
+		
+		Random random = new Random();
+		for(int i = 0 ;i < opCount ; i++){
+			stack.push(random.nextInt(Integer.MAX_VALUE));
+		}
+		
+		for(int i = 0 ;i < opCount ; i++){
+			stack.pop();
+		}
+		
+		long endTime = System.nanoTime();
+		return (endTime-startTime)/1000000000.0;
+	}
+	
+	public static void test3(){
+		
+		int opCount = 10000000;
+		
+		ArrayStack<Integer> arrayStack = new ArrayStack<Integer>();
+		double time1 = testStack(arrayStack,opCount);
+		System.out.println("ArrayStack, time:" + time1 + "s");
+		
+		LinkedListStack<Integer> linkedListStack = new LinkedListStack<Integer>();
+		double time2 = testStack(linkedListStack,opCount);
+		System.out.println("LinkedListStack, time:" + time2 + "s");
+		
+		
+	}
+	
 }
