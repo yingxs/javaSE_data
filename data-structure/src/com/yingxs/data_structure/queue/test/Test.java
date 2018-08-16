596d9b0fa250873be22b472bb73bc6030c411b1d
王昊 <yingxs888@126.com>
2018-08-16 20:22:44 +0800

    feat (LinkedListQueue):使用带尾指针的链表实现队列,时间复杂度O(1)
    经过实测loopQueue和linkedListQueue性能无较大差异，当数据量过大时loopQueue的性能略高于linkedListQueue，这可能与linkedListQueue底层new操作有关

--- data-structure/src/com/yingxs/data_structure/queue/test/Test.java
+++ data-structure/src/com/yingxs/data_structure/queue/test/Test.java
@@ -3,6 +3,7 @@ package com.yingxs.data_structure.queue.test;
 import java.util.Random;
 
 import com.yingxs.data_structure.queue.ArrayQueue;
+import com.yingxs.data_structure.queue.LinkedListQueue;
 import com.yingxs.data_structure.queue.LoopQueue;
 import com.yingxs.data_structure.queue.Queue;
 
@@ -11,20 +12,39 @@ public class Test {
 //		test1();
 //		test2();
 		test3();
+//		test4();
+	}
+	
+	
+	public static void test4(){
+		LinkedListQueue<Integer> queue = new LinkedListQueue<Integer>();
+		for(int i = 0 ;i < 10 ;i++){
+			queue.enqueue(i);
+			System.out.println(queue);
+			
+			if( i % 3 == 2){
+				queue.dequeue();
+				System.out.println(queue);
+			}
+		}
 	}
 	
 	
 	public static void test3(){
 		
-		int opCount = 100000;
+		int opCount = 10000000;
 		
-		ArrayQueue<Integer> arrayQueue = new ArrayQueue<Integer>();
-		double time1 = testQueue(arrayQueue, opCount);
-		System.out.println("ArrayQueue, time: "+time1+" s");
+//		ArrayQueue<Integer> arrayQueue = new ArrayQueue<Integer>();
+//		double time1 = testQueue(arrayQueue, opCount);
+//		System.out.println("ArrayQueue, time: "+time1+" s");
 		
 		LoopQueue<Integer> loopQueue = new LoopQueue<Integer>();
 		double time2 = testQueue(loopQueue, opCount);
 		System.out.println("LoopQueue, time: "+time2+" s");
+		
+		LinkedListQueue<Integer> linkedListQueue = new LinkedListQueue<Integer>();
+		double time3 = testQueue(linkedListQueue, opCount);
+		System.out.println("LinkedListQueue, time: "+time3+" s");
 		
 		
 	}
