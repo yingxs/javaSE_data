5170c67ff6265fc93650c7d5f4fc675001d9b24b
王昊 <yingxs888@126.com>
2018-08-18 22:09:18 +0800

    feat (recursion):增加输出语句调试递归删除链表元素算法

--- data-structure/src/com/yingxs/data_structure/linkedList/Solution.java
+++ data-structure/src/com/yingxs/data_structure/linkedList/Solution.java
@@ -31,7 +31,7 @@ public class Solution {
 		ListNode head = new ListNode(nums);
 		System.out.println(head);
 		
-		ListNode res =  new Solution3().removeElements(head, 6);
+		ListNode res =  new Solution4().removeElements(head, 6,0);
 		System.out.println(res);
 	}
 }
@@ -78,6 +78,41 @@ public class Solution {
 		return head.val == val ? head.next :head;
 		
 		
+	 }
+ }
+ 
+ 
+ class Solution4 {
+	 public ListNode removeElements(ListNode head , int val,int depth){
+		 String depthString = generateDepthString(depth);
+		 System.out.print(depthString);
+		 System.out.println("Call:remove "+val+" in "+head);
+		 if(head == null){
+		 	System.out.print(depthString);
+		 	System.out.println("Return: "+head);
+			 return head;
+		 }
+		 ListNode res = removeElements(head.next, val,depth+1);
+		 System.out.print(depthString);
+		 System.out.println("After remove "+val+": "+res);
+		 
+		 ListNode ret;
+		 if(head.val == val)
+			 ret = res;
+		 else{
+			 head.next = res;
+			 ret = head;
+		 }
+		 System.out.print(depthString);
+	 	System.out.println("Return: "+ret);
+		 return ret;
+	 }
+	 
+	 private String generateDepthString(int depth){
+		 StringBuilder res = new StringBuilder();
+		 for(int i = 0 ;i <depth ;i ++)
+			 res.append("--");
+		 return res.toString();
 	 }
  }
 
