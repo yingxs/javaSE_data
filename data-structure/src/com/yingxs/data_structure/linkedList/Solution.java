0319ebb7a6642ecd4a3760b99a18c8e7c7fab8d2
王昊 <yingxs888@126.com>
2018-08-18 19:25:48 +0800

    javaSE_02.md

--- data-structure/src/com/yingxs/data_structure/linkedList/Solution.java
+++ data-structure/src/com/yingxs/data_structure/linkedList/Solution.java
@@ -32,11 +32,11 @@ public class Solution {
 	
 	
 	public static void main(String[] args) {
-		int[] nums = {1,2,6,3,4,5,6};
+		int[] nums = {6,6,1,1,2,6,3,4,5,6,6};
 		ListNode head = new ListNode(nums);
 		System.out.println(head);
 		
-		ListNode res =  new Solution2().removeElements(head, 6);
+		ListNode res =  new Solution().removeElements(head, 6);
 		System.out.println(res);
 	}
 }
