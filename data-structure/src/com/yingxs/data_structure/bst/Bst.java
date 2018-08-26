9dee4015da1646f8bc3b9d6f7bfe70c5295314cf
王昊 <yingxs888@126.com>
2018-08-26 18:56:07 +0800

    feat (bst):二分搜索树的前序遍历

--- data-structure/src/com/yingxs/data_structure/bst/Bst.java
+++ data-structure/src/com/yingxs/data_structure/bst/Bst.java
@@ -89,6 +89,82 @@ public class Bst<E extends Comparable<E>> {
 		
 	}
 	
+	/**
+	 * 看二分搜索树中是否包含元素e
+	 * @param e
+	 * @return
+	 */
+	public boolean contains(E e){
+		return contains(root,e);
+	}
+	
+	/**
+	 * 查看以node为根的二分搜索树中是否包含元素e,递归算法
+	 * @param node
+	 * @param e
+	 * @return
+	 */
+	private boolean contains(Node node,E e){
+		
+		if(node == null)
+			return false;
+		
+		if( e.compareTo(node.e) == 0)
+			return true;
+		else if(e.compareTo(node.e) < 0)
+			return contains(node.left,e);
+		else
+			return contains(node.right,e);
+	}
+	
+	/**
+	 * 前序遍历
+	 */
+	public void preOrder(){
+		preOrder(root);
+	}
+	
+	/**
+	 * 前序遍历以node为根的二分搜索树，递归算法
+	 * @param node
+	 */
+	private void preOrder(Node node){
+		
+		if(node == null)
+			return ;
+		
+		System.out.println(node.e);
+		preOrder(node.left);
+		preOrder(node.right);
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder res = new StringBuilder();
+		generateBSTString(root,0,res);
+		return res.toString();
+	}
+	
+	private void generateBSTString(Node node,int depth,StringBuilder res){
+		
+		if(node == null ){
+			res.append(generateDepthString(depth) + "null\n");
+			return ;
+		}
+		
+		res.append(generateDepthString(depth)+node.e+"\n");
+		generateBSTString(node.left,depth+1,res);
+		generateBSTString(node.right,depth+1,res);
+		
+	}
+	
+	
+	private String generateDepthString(int depth){
+		StringBuilder res = new StringBuilder();
+		for(int i = 0 ;i < depth ; i++)
+			res.append("--");
+		return res.toString();
+	}
 	
 	
 	
---
+++ data-structure/src/com/yingxs/data_structure/bst/test/Test.java
@@ -0,0 +1,18 @@
+package com.yingxs.data_structure.bst.test;
+
+import com.yingxs.data_structure.bst.Bst;
+
+public class Test {
+	public static void main(String[] args) {
+		Bst<Integer> bst = new Bst<Integer>();
+		
+		int[] nums = {5,3,6,8,4,2};
+		for(int num :nums)
+			bst.add(num);
+				
+		bst.preOrder();
+		System.out.println();
+		System.out.println(bst);
+		
+	}
+}
