0bb45dfe8465628212a6e077aaaf6420b0525f0e
王昊 <yingxs888@126.com>
2018-08-27 21:16:02 +0800

    feat (bst):二分搜索树中删除最大值和最小值

--- data-structure/src/com/yingxs/data_structure/bst/Bst.java
+++ data-structure/src/com/yingxs/data_structure/bst/Bst.java
@@ -53,9 +53,6 @@ public class Bst<E extends Comparable<E>> {
 		
 		root = add(root,e);
 		
-		
-		
-		
 	}
 	
 	/**
@@ -221,6 +218,105 @@ public class Bst<E extends Comparable<E>> {
 			if(cur.right!=null)
 				q.add(cur.right);
 		}
+	}
+	
+	/**
+	 * 寻找二分搜索树的最小元素
+	 */
+	public E minimum(){
+		if(size == 0)
+			throw new IllegalArgumentException("BST is empty!");
+		return minimum(root).e;
+	}
+
+	/**
+	 * 返回以node为根的二分搜索树的最小值所在的节点
+	 * @param node
+	 * @return
+	 */
+	private Node minimum(Node node){
+		if(node.left == null)
+			return node;
+		return minimum(node.left);
+	}
+	/**
+	 * 寻找二分搜索树的最大元素
+	 */
+	public E maximum(){
+		if(size == 0)
+			throw new IllegalArgumentException("BST is empty!");
+		return maximum(root).e;
+	}
+	
+	/**
+	 * 返回以node为根的二分搜索树的最大值所在的节点
+	 * @param node
+	 * @return
+	 */
+	private Node maximum(Node node){
+		if(node.right == null)
+			return node;
+		return minimum(node.right);
+	}
+	
+	/**
+	 * 从二分搜索树中删除最小值所在节点，返回最小值
+	 * @return
+	 */
+	public E removeMin(){
+		E ret = minimum();
+		root = removeMin(root);
+		return ret;
+	}
+	
+	
+	
+	/**
+	 * 删除掉以node为根的二分搜索树中的最小节点
+	 * @param node
+	 * @return 返回删除节点后新的二分搜索树的根
+	 */
+	private Node removeMin(Node node) {
+		if(node.left == null){
+			Node rightNode = node.right;
+			node.right = null;
+			size --;
+			return rightNode;
+		}
+		
+		node.left = removeMin(node.left);
+		return node;
+		
+	}
+	/**
+	 * 从二分搜索树中删除最大值所在节点，返回最大值
+	 * @return
+	 */
+	public E removeMax(){
+		E ret = maximum();
+		root = removeMax(root);
+		return ret;
+	}
+	
+	
+	
+	/**
+	 * 删除掉以node为根的二分搜索树中的最大节点
+	 * @param node
+	 * @return 返回删除节点后新的二分搜索树的根
+	 */
+	private Node removeMax(Node node) {
+		
+		if(node.right == null){
+			Node leftNode = node.left;
+			node.left = null;
+			size --;
+			return leftNode;
+		}
+		
+		node.right = removeMax(node.right);
+		return node;
+		
 	}
 
 	@Override
--- data-structure/src/com/yingxs/data_structure/bst/test/Test.java
+++ data-structure/src/com/yingxs/data_structure/bst/test/Test.java
@@ -1,9 +1,58 @@
 package com.yingxs.data_structure.bst.test;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 import com.yingxs.data_structure.bst.Bst;
 
 public class Test {
 	public static void main(String[] args) {
+//		test1();
+		
+		test2();
+		test3();
+		 
+	}
+
+	private static void test3() {
+		Bst<Integer> bst = new Bst<Integer>();
+		Random random = new Random();
+		 int n = 1000;
+		 
+		 for(int i = 0 ;i < n ; i++)
+			 bst.add(random.nextInt(10000));
+		
+		 ArrayList<Integer> nums = new ArrayList<Integer>();
+		 while(!bst.isEmpty())
+			 nums.add(bst.removeMax());
+		 System.out.println(nums);
+		 
+		 for(int i = 1 ; i<nums.size();i++)
+			 if(nums.get(i-1) < nums.get(i))
+				 throw new IllegalArgumentException("Error");
+		 System.out.println("removeMin test completed");
+	}
+
+	private static void test2() {
+		Bst<Integer> bst = new Bst<Integer>();
+		Random random = new Random();
+		 int n = 1000;
+		 
+		 for(int i = 0 ;i < n ; i++)
+			 bst.add(random.nextInt(10000));
+		
+		 ArrayList<Integer> nums = new ArrayList<Integer>();
+		 while(!bst.isEmpty())
+			 nums.add(bst.removeMin());
+		 System.out.println(nums);
+		 
+		 for(int i = 1 ; i<nums.size();i++)
+			 if(nums.get(i-1) > nums.get(i))
+				 throw new IllegalArgumentException("Error");
+		 System.out.println("removeMin test completed");
+	}
+
+	private static void test1() {
 		Bst<Integer> bst = new Bst<Integer>();
 		
 		int[] nums = {5,3,6,8,4,2};
@@ -23,6 +72,5 @@ public class Test {
 //		
 //		bst.postOrder();
 //		System.out.println(bst);
-		
 	}
 }
