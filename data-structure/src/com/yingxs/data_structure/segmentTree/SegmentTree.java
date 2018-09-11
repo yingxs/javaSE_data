064ab79b862f4f622298ae79e519f32dacfffa69
王昊 <yingxs888@126.com>
2018-09-11 20:04:43 +0800

    feat (SegmentTree):线段树的基础表示

---
+++ data-structure/src/com/yingxs/data_structure/segmentTree/SegmentTree.java
@@ -0,0 +1,38 @@
+package com.yingxs.data_structure.segmentTree;
+
+public class SegmentTree<E> {
+	
+	private E[] tree;
+	private E[] data;
+	
+	public SegmentTree(E[] arr){
+		this.data = arr;
+		
+		/*data = (E[]) new Object[arr.length];
+		for(int i = 0 ;i < arr.length ; i ++)
+			data[i] = arr[i];*/
+		
+		tree = (E[]) new Object[4*arr.length];
+	}
+	
+	public int getSize(){
+		return data.length;
+	}
+	
+	public E get(int index){
+		if(index < 0 ||index >= data.length)
+			throw new IllegalArgumentException("Index is illegal.");
+		return data[index];
+	}
+	
+	
+	private int leftChild(int index){
+		return 2*index+1;
+	}
+	
+	private int rightChild(int index){
+		return 2*index+2;
+	}
+	
+	
+}
